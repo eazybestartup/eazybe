f09ca215eca27b4b1dd373a404848362875424b4
gneves <gneves@mmcafe.com.br>
2019-07-03 15:18:21 -0300

    ajuste atributos do usuário

--- eazybe-api/db/migrate/20190702220547_create_users.rb
+++ eazybe-api/db/migrate/20190702220547_create_users.rb
@@ -1,11 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.text :name
+      t.text :full_name
       t.text :last_name
       t.text :email
       t.text :password_digest
       t.date :birth_date
+      t.string :cep
+      t.string :street
+      t.string :number
+      t.string :district
+      t.string :complement
+      t.string city
 
       t.timestamps
     end
