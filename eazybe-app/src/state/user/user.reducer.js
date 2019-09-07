1837dd9255904e150f7dc84b7b5a1f0c368197e7
Lucas Muniz Dutra <lucasmunizdutra@MacBook-Air-de-Lucas.local>
2019-09-07 14:32:45 -0300

    reducer

---
+++ eazybe-app/src/state/user/user.reducer.js
@@ -0,0 +1,18 @@
+import {
+    SET_USER,
+    FINISH_GET_USER_BY_ID
+  } from './user.actionTypes';
+  
+  const INITIAL_STATE = {};
+  
+  const userReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+      case SET_USER:
+        return { ...action.user }
+      case FINISH_GET_USER_BY_ID:
+      default:
+        return state;
+    }  
+  };
+  
+  export default userReducer;
